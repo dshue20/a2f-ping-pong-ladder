e8095cb96ecb9aca92b24adc4dca85a1c6c86f01
dshue20 <dshue@berkeley.edu>
2026-01-31 12:25:18 -0800

    edits

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,21 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAFApBh1bXAByvMpzktUu17F6uyis6HYoQ",
-  authDomain: "ping-pong-ladder-fa1bc.firebaseapp.com",
-  projectId: "ping-pong-ladder-fa1bc",
-  storageBucket: "ping-pong-ladder-fa1bc.firebasestorage.app",
-  messagingSenderId: "1072641443184",
-  appId: "1:1072641443184:web:57df4b5daee1111f107bb5",
-  measurementId: "G-E9FJ3WJ19N",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
